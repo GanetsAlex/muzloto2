60bfb9fc874743a0fadde843277bcb7c56b24994
GanetsAlex <ganetsalex@gmail.com>
2025-12-30 11:08:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-const CACHE_NAME = 'music-loto-v1';
+const CACHE_NAME = 'music-loto-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/muzloto2/',
+  '/muzloto2/index.html',
+  '/muzloto2/style.css',
+  '/muzloto2/app.js',
+  '/muzloto2/manifest.json',
+  '/muzloto2/icon-192.png',
+  '/muzloto2/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
